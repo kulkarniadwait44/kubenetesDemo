015ac7a4281e9cc79a551289790e995c193ccfa7
kulkarniadwait44 <kulkarniadwait44@gmail.com>
2021-07-22 13:07:21 +0530

    Update DeployDemo.sh

--- DeployDemo.sh
+++ DeployDemo.sh
@@ -4,7 +4,3 @@ export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
 ./mvnw package && java -jar target/demo
 docker build -t demo .
 docker login -u "kulkarniadwait44" -p "Mimo#1234"
-<<<<<<< HEAD
-
-=======
->>>>>>> a8c4c229e3a568b156633edbf576c086047c57bd
