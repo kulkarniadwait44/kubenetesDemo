160f339cfb3095460ae6d3a60198701be3f30941
kulkarniadwait44 <kulkarniadwait44@gmail.com>
2021-07-22 12:54:51 +0530

    new config map changes

--- demo/src/main/java/com/ak/demo/DemoApplication.java
+++ demo/src/main/java/com/ak/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.ak.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
+	
+	@Value("${APPLICATION_PROPERTIES_HOSTNAME}")
+	String hostName;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@RequestMapping("/hello")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Greetings from Spring Boot!"+hostName;
 	}
 }
