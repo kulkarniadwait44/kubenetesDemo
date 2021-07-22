1667198119f5e1741c778c5f51aa9fc1f2febff7
kulkarniadwait44 <kulkarniadwait44@gmail.com>
2021-07-22 10:53:56 +0530

    Create DeployDemo.sh

---
+++ DeployDemo.sh
@@ -0,0 +1,7 @@
+cd demo
+chmod +x ./mvnw
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+./mvnw package && java -jar target/demo
+docker build -t demo .
+docker login -u "kulkarniadwait44" -p "Mimo#1234"
+docker push kulkarniadwait44/demo:1.0.0
