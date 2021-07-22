eb63c0228f075c208e4ed9e122c66acad41f1ccf
kulkarniadwait44 <kulkarniadwait44@gmail.com>
2021-07-22 13:50:47 +0530

    new config map changes

--- demo/src/main/java/com/ak/demo/DemoApplication.java
+++ demo/src/main/java/com/ak/demo/DemoApplication.java
@@ -3,11 +3,13 @@ package com.ak.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient 
 public class DemoApplication {
 	
 	@Value("${APPLICATION_PROPERTIES_HOSTNAME}")
