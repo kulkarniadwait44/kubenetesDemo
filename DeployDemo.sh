d3cf25950d2d47bd42ddf83d1be111e643dc2894
kulkarniadwait44 <kulkarniadwait44@gmail.com>
2021-07-23 12:02:46 +0530

    Update DeployDemo.sh

--- DeployDemo.sh
+++ DeployDemo.sh
@@ -1,7 +1,6 @@
 cd demo
 chmod +x ./mvnw
 export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
-export APPLICATION_PROPERTIES_HOSTNAME=""
 ./mvnw package && java -jar target/demo
 docker build -t demo .
 docker login -u "kulkarniadwait44" -p "Mimo#1234"
@@ -10,4 +9,14 @@ docker push kulkarniadwait44/demo:1.0.0
 cd /demo
 kubectl apply -f service.yaml
 kubectl apply -f deployments.yaml
-kubectl expose deployment demo --type=NodePort --port=8085 --name=demo-service
+#kubectl expose deployment demo --type=NodePort --port=8085 --name=demo-svc
+cd ..
+cd /demo-caller
+chmod +x ./mvnw
+./mvnw package && java -jar target/demo-caller
+docker build -t demo-caller .
+docker tag $(docker images demo-caller --format "{{.ID}}") kulkarniadwait44/demo-caller:1.0.0
+docker push kulkarniadwait44/demo-caller:1.0.0
+kubectl apply -f service.yaml
+kubectl apply -f deployments.yaml
+#kubectl expose deployment demo-caller --type=NodePort --port=8085 --name=demo-svc
