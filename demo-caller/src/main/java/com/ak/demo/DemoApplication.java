9020a7a44a47d914924b7332732a0dd4540cc0df
kulkarniadwait44 <kulkarniadwait44@gmail.com>
2021-07-23 12:04:39 +0530

    demo-caller service push

--- demo-caller/src/main/java/com/ak/demo/DemoApplication.java
+++ demo-caller/src/main/java/com/ak/demo/DemoApplication.java
@@ -3,12 +3,14 @@ package com.ak.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient 
 public class DemoApplication {
 	
 	@Autowired
