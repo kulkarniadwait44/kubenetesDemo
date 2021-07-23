559a4c3f704662e21baeae520e290eb5a6006743
kulkarniadwait44 <kulkarniadwait44@gmail.com>
2021-07-23 11:11:01 +0530

    Update DeployDemo.sh

--- DeployDemo.sh
+++ DeployDemo.sh
@@ -7,6 +7,7 @@ docker build -t demo .
 docker login -u "kulkarniadwait44" -p "Mimo#1234"
 docker tag $(docker images demo --format "{{.ID}}") kulkarniadwait44/demo:1.0.0
 docker push kulkarniadwait44/demo:1.0.0
+cd demo
 docker apply -f service.yaml
 docker apply -f deployment.yaml
 kubectl expose deployment demo --type=NodePort --port=8085 --name=demo-service
