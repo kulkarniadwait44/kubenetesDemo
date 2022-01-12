4e9d5703890cc1a8fd266ef414b0f964e578e9c4
kulkarniadwait44 <kulkarniadwait44@gmail.com>
2022-01-12 19:03:28 +0530

    New RedisLetteuceSpringWritter

---
+++ RedisLetteuceSpringWritter/src/main/java/com/redis/RedisLetteuceSpringWritterApplication.java
@@ -0,0 +1,13 @@
+package com.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLetteuceSpringWritterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLetteuceSpringWritterApplication.class, args);
+	}
+
+}
